bd2a1e561af91e054626b04d1a51c9368586ac59
Leo Wilkin <leo.wilkin10@gmail.com>
2023-10-06 10:32:02 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-    const CHATGPT_API_KEY = "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";
+    const CHATGPT_API_KEY = "nice try buster";
     const CHATGPT_API_URL = "https://api.openai.com/v1/chat/completions";
     const PROMPT = "You are a friendly, conversational chatbot named lkwGPT. Your aim is to assist your user to the best of your ability, while being very positive and friendly.";
 
